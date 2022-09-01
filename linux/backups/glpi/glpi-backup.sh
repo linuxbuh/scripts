567c02f84a06b8c6101da2dcf3c23cb61a7cb7e1
Захарчук Тимофей Викторович <t.zaharchuk@AVK-Company.local>
2022-09-01 15:54:43 +0300

    Update glpi-backup.sh

--- linux/backups/glpi/glpi-backup.sh
+++ linux/backups/glpi/glpi-backup.sh
@@ -15,11 +15,12 @@ TODAY=`date +"%d%b%Y"`
 ################## Update below values ########################
 
 BACKUP_PATH='/opt/backup/glpi'
+GLPI_PATH=''
 MYSQL_HOST='localhost'
 MYSQL_PORT='3306'
-MYSQL_USER='dbuzabbix'
-MYSQL_PASSWORD='eShie5oGiw'
-DATABASE_NAME='db4zabbix'
+MYSQL_USER=''
+MYSQL_PASSWORD=''
+DATABASE_NAME=''
 BACKUP_RETAIN_DAYS=30 ## Number of days to keep local backup copy
 
 #################################################################
@@ -27,22 +28,10 @@ BACKUP_RETAIN_DAYS=30 ## Number of days to keep local backup copy
 ##### Backups files #####
 
 mkdir -p ${BACKUP_PATH}
-mkdir -p ${BACKUP_PATH}/${TODAY}/etc
-mkdir -p ${BACKUP_PATH}/${TODAY}/etc/zabbix
-mkdir -p ${BACKUP_PATH}/${TODAY}/etc/nginx
-mkdir -p ${BACKUP_PATH}/${TODAY}/etc/php-fpm.d
-mkdir -p ${BACKUP_PATH}/${TODAY}/etc/php.d
-mkdir -p ${BACKUP_PATH}/${TODAY}/etc/httpd
-mkdir -p ${BACKUP_PATH}/${TODAY}/usr
-mkdir -p ${BACKUP_PATH}/${TODAY}/usr/share
-mkdir -p ${BACKUP_PATH}/${TODAY}/usr/share/zabbix
+mkdir -p ${BACKUP_PATH}/www
 
-cp -f -R /etc/zabbix/* ${BACKUP_PATH}/${TODAY}/etc/zabbix/
-cp -f -R /etc/nginx/* ${BACKUP_PATH}/${TODAY}/etc/nginx/
-cp -f -R /etc/php-fpm.d/* ${BACKUP_PATH}/${TODAY}/etc/php-fpm.d/
-cp -f -R /etc/php.d/* ${BACKUP_PATH}/${TODAY}/etc/php.d/
-cp -f -R /etc/httpd/* ${BACKUP_PATH}/${TODAY}/etc/httpd/
-cp -f -R /usr/share/zabbix/* ${BACKUP_PATH}/${TODAY}/usr/share/zabbix/
+cp -f -R $GLPI_PATH/* ${BACKUP_PATH}/${TODAY}/www
+
 
 if [ $? -eq 0 ]; then
 echo "Files backup successfully completed"
